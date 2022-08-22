55aabadc6101208b76e40090ea6c9ee1298e516e
tomasyovino <tomasyovino@gmail.com>
2022-08-21 21:38:05 -0300

    Add Shop & Blog pages

--- JS/main.js
+++ JS/main.js
@@ -13,3 +13,21 @@ if (closeBtn) {
         nav.classList.remove("active");
     });
 };
+
+/* Shop Product P */
+// Change img src
+let mainImg = document.getElementById('mainImg');
+let smallImg = document.getElementsByClassName('sm-img');
+
+smallImg[0].onclick = () => {
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = () => {
+    mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = () => {
+    mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = () => {
+    mainImg.src = smallImg[3].src;
+}
